6c4dd0cba07454aa8b913ef0f89639c393a4d2d8
Subigya Lamichhane <subigyalamichhane@gmail.com>
2023-03-22 23:15:38 +0545

    Added PWA functionality and basic notification

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f4bfb94fd8b6a90e"; }
+/******/ 		__webpack_require__.h = function() { return "b686bef79c24cd7e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
