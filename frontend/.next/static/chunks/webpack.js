9a4313d809d95adc255ab3c577be17b81e5a0b98
swarupdev <121184771+swarupdev@users.noreply.github.com>
2023-04-02 04:36:21 +0000

    added full dockerization

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cd154623541e4d16"; }
+/******/ 		__webpack_require__.h = function() { return "f84558e08ae8c4f6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
